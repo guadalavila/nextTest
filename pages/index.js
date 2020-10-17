ad300792739fe5911246fa88d4f6e40af341ae74
Guadalupe <galavila@everis.com>
2020-10-17 15:00:37 -0300

    Primer commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  function goToNotification() {
+    try {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          back: false,
+          label: "se ejecuto boton Notificaciones",
+        })
+      );
+    } catch (e) {}
+  }
+  function showAlert() {
+    try {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ back: true, label: "se ejecuto boton alerta" })
+      );
+    } catch (e) {}
+  }
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+          <div style={{ marginTop: 30, marginBottom: 30 }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+          <button
+            style={{
+              backgroundColor: "#0B2739",
+              height: 50,
+              marginBottom: 20,
+              width: 300,
+              borderRadius: 50,
+            }}
+            onClick={goToNotification}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            <span style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+              Ir a notificaciones
+            </span>
+          </button>
+          <button
+            style={{
+              backgroundColor: "#019df4",
+              height: 50,
+              marginBottom: 20,
+              width: 300,
+              borderRadius: 50,
+            }}
+            onClick={showAlert}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <span style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+              Ver alerta
+            </span>
+          </button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
