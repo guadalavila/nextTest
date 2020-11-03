00c046eee8f44f20f491859509c7789b04d42bf9
Guadalupe <galavila@everis.com>
2020-11-03 20:34:52 -0300

    mock

---
+++ utils/mock.js
@@ -0,0 +1,18 @@
+export const facturas = [
+  {
+    title: "Factura 10-2020",
+    periodo: "10-2020",
+  },
+  {
+    title: "Factura 09-2020",
+    periodo: "09-2020",
+  },
+  {
+    title: "Factura 08-2020",
+    periodo: "08-2020",
+  },
+  {
+    title: "Factura 07-2020",
+    periodo: "07-2020",
+  },
+];
